57056dd8f166946d9877f3543dcfbd8744359023
Cauã Ciconelli <99812511+CauaCiconelli@users.noreply.github.com>
2022-11-18 12:08:39 -0300

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -11,6 +11,6 @@ sudo systemctl start docker
 sudo systemctl enable docker
 fi
 docker pull mysql
-sudo docker run -d -p 3305:3305 --name bancoRabbit -e "MYSQL_DATABASE=bolsa" -e "MYSQL_ROOT_PASSWORD=bolsa" mysql
+sudo docker run -d -p 3300:3306 --name bancoRabbit -e "MYSQL_DATABASE=bolsa" -e "MYSQL_ROOT_PASSWORD=bolsa" mysql
 cd /home/scripts/repositorioJavaAWS
 sudo docker exec -it bancoRabbit bash
